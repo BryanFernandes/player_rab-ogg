6cc341df625b2807dee45b276677504f5766c92a
BryanFernandes <bryan.h.fernandes@hotmail.com>
2018-07-27 03:48:14 -0300

    fix script to install virtualbox

--- virtualbox_install.sh
+++ virtualbox_install.sh
@@ -11,7 +11,4 @@
 #                apt-get install -y virtualbox-5.0
 #        fi
 #fi
-sudo sh -c "echo 'deb http://download.virtualbox.org/virtualbox/debian 
-'$(lsb_release -cs)' contrib non-free' > /etc/apt/sources.list.d/virtualbox.list" && 
-wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | sudo apt-
-key add - && sudo apt-get update && sudo apt-get install virtualbox-4.3 dkms
+sudo sh -c "echo 'deb http://download.virtualbox.org/virtualbox/debian '$(lsb_release -cs)' contrib non-free' > /etc/apt/sources.list.d/virtualbox.list" && wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | sudo apt-key add - && sudo apt-get update && sudo apt-get install virtualbox-4.3 dkms
